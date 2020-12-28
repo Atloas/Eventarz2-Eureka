18095d84d174cb1825e890a580fe3fd4c78531c1
Atloas <miter010@gmail.com>
2020-12-28 16:36:25 +0100

    Basic server done

--- README.md
+++
@@ -1,1 +0,0 @@
-# Eventarz2-Eureka
---
+++ src/test/java/com/agh/Eventarz2Eureka/Eventarz2EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.Eventarz2Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eventarz2EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
